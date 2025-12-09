ca392ed88649ee623d999a564250d0224491195b
Jokec <bank13946@gmail.com>
2025-12-09 14:02:17 +0700

    base form backup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,41 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Lobby from './Lobby';
-import XO from './games/XO';
+{/*import XO from './games/XO';
 import FruitCut from './games/FruitCut';
 import Dino from './games/Dino';
 import SnakeGame from './games/SnakeGame';
-
+*/}
 export default function App() {
   return (
     <BrowserRouter>
       <header className="p-4 border-b flex justify-between">
-        <Link to="/" className="font-bold">MiniGame</Link>
+        <Link to="/" className="font-bold">Futiger</Link>
         <nav className="space-x-4">
+
+
+
+
+          {/*
           <Link to="/xo">XO</Link>
           <Link to="/fruitcut">Fruit Cut</Link>
           <Link to="/dino">Dino</Link>
           <Link to="/snakegame">Snake Game</Link>
+          */}
         </nav>
       </header>
       <main className="p-4">
         <Routes>
           <Route path="/" element={<Lobby />} />
+
+
+
+
+          {/*
           <Route path="/xo" element={<XO />} />
           <Route path="/fruitcut" element={<FruitCut />} />
           <Route path="/dino" element={<Dino />} />
           <Route path="/snakegame" element={<SnakeGame />} />
+          */}
         </Routes>
       </main>
     </BrowserRouter>
--- frontend/src/Lobby.jsx
+++ frontend/src/Lobby.jsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 export default function Lobby() {
   return (
     <div className="max-w-xl mx-auto mt-10 space-y-4">
-      <h1 className="text-2xl font-bold text-center">🎮 MiniGame Lobby</h1>
+      <h1 className="text-2xl font-bold text-center">Welcome to Futiger</h1>
       <div className="grid grid-cols-1 gap-4">
-        <Link className="card" to="/xo"><span className="font-semibold">Tic-Tac-Toe (XO)</span> — Multiplayer & Bot</Link>
-        <Link className="card" to="/fruitcut"><span className="font-semibold">Fruit Cut</span> — Slice fruits, score up!</Link>
+       
+       
+       
+       {/* <Link className="card" to="/xo"><span className="font-semibold">Tic-Tac-Toe (XO)</span> — Multiplayer & Bot</Link>
+       <Link className="card" to="/fruitcut"><span className="font-semibold">Fruit Cut</span> — Slice fruits, score up!</Link>
         <Link className="card" to="/dino"><span className="font-semibold">Dino Run</span> — Jump to survive</Link>
-        <Link className="card" to="/snakegame"><span className="font-semibold">Snake Game</span> — Eat the apple</Link>
+        <Link className="card" to="/snakegame"><span className="font-semibold">Snake Game</span> — Eat the apple</Link>*/}
       </div>
     </div>
   );
